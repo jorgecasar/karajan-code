52f69d12eef4b8eec7e961ba61c35147ed23672e
manufosela <manufosela@gmail.com>
2026-02-28 23:59:17 +0100

    chore: bump version to 1.2.1 and include Spanish README in npm package

--- src/cli.js
+++ src/cli.js
@@ -24,7 +24,7 @@ async function withConfig(commandName, flags, fn) {
 }
 
 const program = new Command();
-program.name("kj").description("Karajan Code CLI").version("1.2.0");
+program.name("kj").description("Karajan Code CLI").version("1.2.1");
 
 program
   .command("init")
